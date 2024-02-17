7584dde2567f9c6fe0e85c4d6c849f5c97de2552
niharika1102 <niharikag1102@gmail.com>
2024-02-17 16:25:13 +0530

    Additional Functionality
    Added strikethrough and display full array functionality

--- RandomNumberGenerator.js
+++ RandomNumberGenerator.js
@@ -1,14 +1,22 @@
 const limit = prompt("Enter last number: ");
 const nums = [];
 const numHere = document.getElementById('numHere');
+const numArray = document.getElementById('numArray');
 
 for (let i = 0; i < limit; i++) {
     nums.push(i + 1);
 }
 
+// Function to create HTML with strikethrough
+function createNumberHTML(number) {
+    return `<span id="number-${number}" class="number">${number}</span>`;
+}
+
+// Render the numbers array
+numArray.innerHTML = nums.map(createNumberHTML).join('');
+
 function randomNum() {
     if (nums.length == 0) {
-        // console.log("All nums done!!");
         numHere.innerHTML = "All numbers done!!";
         return;
     }
@@ -16,12 +24,11 @@ function randomNum() {
     let randomIndex = Math.floor(Math.random() * nums.length);
     let random = nums[randomIndex];
 
-    numHere.innerHTML = random;
+    // Apply strikethrough
+    document.getElementById(`number-${random}`).classList.add('strikethrough');
 
-    // console.log(random);
+    numHere.innerHTML = random;
     nums.splice(randomIndex, 1);
-    // console.log(nums);
 }
 
 randomNum();
-// console.log(nums);
